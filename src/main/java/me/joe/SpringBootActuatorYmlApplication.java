087cc406d5cbb3b96603d6a1f6cb8910a6924b6f
JoeKo <a125908571@gmail.com>
2021-07-23 16:43:34 +0800

    initial commit of full repository

---
+++ src/main/java/me/joe/SpringBootActuatorYmlApplication.java
@@ -0,0 +1,13 @@
+package me.joe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorYmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorYmlApplication.class, args);
+	}
+
+}
